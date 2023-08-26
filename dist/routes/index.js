5292cf8edac7da9df52fce40b4a535d438b8fb6f
ForkEyeee <shawncarter123456@gmail.com>
2023-08-26 19:20:07 -0400

    removed console.logs

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -3,36 +3,34 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const router = express.Router();
 const messages = [
-    {
-        text: "Hi there!",
-        user: "Amando",
-        added: new Date(),
-    },
-    {
-        text: "Hello World!",
-        user: "Charles",
-        added: new Date(),
-    },
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
 ];
 router.post("/new", function (req, res, next) {
-    messages.push({
-        text: req.body.messageText,
-        user: req.body.authorText,
-        added: new Date(),
-    });
-    res.redirect("/");
-    console.log(messages);
+  messages.push({
+    text: req.body.messageText,
+    user: req.body.authorText,
+    added: new Date(),
+  });
+  res.redirect("/");
 });
 /* GET home page. */
 router.get("/", function (req, res, next) {
-    res.render("index", {
-        title: "Mini Messageboard",
-        messages: messages,
-    });
-    console.log(messages);
+  res.render("index", {
+    title: "Mini Messageboard",
+    messages: messages,
+  });
 });
 router.get("/new", function (req, res, next) {
-    res.render("form", { title: "Add New" });
-    console.log(req.body);
+  res.render("form", { title: "Add New" });
+  console.log(req.body);
 });
 module.exports = { router, messages };
